9ee37a99ecb2ff0fbfed7fade8e384d5133578c8
Sh1503 <shahars5@hotmail.com>
2025-06-22 18:36:38 +0300

    Add files via upload

--- champ.py
+++ champ.py
@@ -260,35 +260,35 @@ def load_league_data():
     data_sources = {
         "Premier League": [
             "https://raw.githubusercontent.com/Sh1503/champ1/main/epl.csv",
-            "https://raw.githubusercontent.com/Sh1503/champ1/main/premier_league_csv.csv"
+            "https://raw.githubusercontent.com/Sh1503/champ1/main/premier_league_csv.csv.txt"
         ],
         "La Liga": [
-            "https://raw.githubusercontent.com/Sh1503/champ1/main/laliga.csv",
-            "https://raw.githubusercontent.com/Sh1503/champ1/main/laliga_csv.csv"
+            "https://raw.githubusercontent.com/Sh1503/champ1/main/laliga.csv.",
+            "https://raw.githubusercontent.com/Sh1503/champ1/main/laliga_csv.csv.txt"
         ],
         "Serie A": [
-            "https://raw.githubusercontent.com/Sh1503/champ1/main/seriea.csv",
-            "https://raw.githubusercontent.com/Sh1503/champ1/main/serie_a_csv.csv"
+            "https://raw.githubusercontent.com/Sh1503/champ1/main/seriea.csv.",
+            "https://raw.githubusercontent.com/Sh1503/champ1/main/serie_a_csv.csv.txt"
         ],
         "Bundesliga": [
-            "https://raw.githubusercontent.com/Sh1503/champ1/main/bundesliga.csv",
-            "https://raw.githubusercontent.com/Sh1503/champ1/main/bundesliga_csv.csv"
+            "https://raw.githubusercontent.com/Sh1503/champ1/main/bundesliga.csv.",
+            "https://raw.githubusercontent.com/Sh1503/champ1/main/bundesliga_csv.csv.txt"
         ],
         "Ligue 1": [
-            "https://raw.githubusercontent.com/Sh1503/champ1/main/ligue1.csv",
-            "https://raw.githubusercontent.com/Sh1503/champ1/main/ligue1_csv.csv"
+            "https://raw.githubusercontent.com/Sh1503/champ1/main/ligue1.csv.",
+            "https://raw.githubusercontent.com/Sh1503/champ1/main/ligue1_csv.csv.txt"
         ],
         "Israeli Premier League": [
-            "https://raw.githubusercontent.com/Sh1503/champ1/main/israeli_premier_league_csv.csv"
+            "https://raw.githubusercontent.com/Sh1503/champ1/main/israeli_premier_league_csv.csv.txt"
         ],
         "Champions League": [
-            "https://raw.githubusercontent.com/Sh1503/champ1/main/champions_league_csv.csv"
+            "https://raw.githubusercontent.com/Sh1503/champ1/main/champions_league_csv.csv.txt"
         ],
         "Europa League": [
-            "https://raw.githubusercontent.com/Sh1503/champ1/main/europa_league_csv.csv"
+            "https://raw.githubusercontent.com/Sh1503/champ1/main/europa_league_csv.csv.txt"
         ],
         "Conference League": [
-            "https://raw.githubusercontent.com/Sh1503/champ1/main/conference_league_csv.csv"
+            "https://raw.githubusercontent.com/Sh1503/champ1/main/conference_league_csv.csv.txt"
         ]
     }
     
